98ca436679e4928b6e70a4362682cd4058d58449
VVarro <virag.varro@gmail.com>
2017-04-18 11:29:59 +0200

    new functions, read in the text to a list, and make a copy of it to the destination

--- copy/src/Copy.java
+++ copy/src/Copy.java
@@ -1,6 +1,12 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Copy {
   public static void main(String[] args) {
@@ -12,13 +18,23 @@ public class Copy {
     // No destination provided
     // When both arguments provided and the source is a file
     // Read all contents from it and write it to the destination
+
+    Copy copy = new Copy();
+    copy.createTextFile();
+    if (args.length == 0) {
+      copy.printOutUsage();
+    } else if (args[0].equals("copy") && args.length == 1) {
+      System.out.println("No destination provided");
+    } else if (args[0].equals("copy") && args.length == 3) {
+      copy.addCopyFile(args,copy.readInText(args));
+    }
   }
 
-  public void printOutUsage() {
+  public static void printOutUsage() {
     System.out.println("copy [source] [destination]");
   }
 
-  public void createTextFile() {
+  public static void createTextFile() {
     PrintWriter writer = null;
     try {
       writer = new PrintWriter("theSourceFile.txt", "UTF-8");
@@ -30,5 +46,32 @@ public class Copy {
     writer.println("The first line");
     writer.println("The second line");
     writer.close();
+  }
+
+  public static List<String> readInText(String[] args) {
+    Path path = Paths.get(args[1]);
+    List<String> originalFile;
+    try {
+      originalFile = Files.readAllLines(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+      originalFile = new ArrayList<>();
+    }
+    return originalFile;
+  }
+
+  public static void addCopyFile(String[] args,List<String> originalFile) {
+    PrintWriter copy = null;
+    try {
+      copy = new PrintWriter(args[2], "UTF-8");
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+    for (int i = 0; i < originalFile.size(); i++) {
+      copy.println(originalFile.get(i));
+    }
+    copy.close();
   }
 }
